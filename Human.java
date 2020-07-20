e2ddbd230237a109e1977f5a5de0586e6f31e3f1
te1638 <stalker.ksielev2012@yandex.ru>
2020-07-20 22:29:10 +0300

    ДЗ 1: Объектно-ориентированное программирование Java

---
+++ Human.java
@@ -0,0 +1,36 @@
+public class Human implements Participant {
+
+    protected String name;
+    protected int jumpHeight;
+    protected int runLength;
+
+    public Human(String name, int jumpHeight, int runLength) {
+        this.name = name;
+        this.jumpHeight = jumpHeight;
+        this.runLength = runLength;
+    }
+
+
+
+    public void run() {
+        System.out.println(String.format("Human [%s] try runing", name));
+
+
+    }
+
+
+    public void jump() {
+        System.out.println(String.format("Human [%s] try jumping", name));
+
+    }
+    public int getJumpHeight(){
+        return jumpHeight;
+    }
+    public int getRunLength(){
+        return runLength;
+    }
+    public void win(){
+        System.out.format("Участник %s завершил полосу препятствий\n", name);
+    }
+
+}
---
+++ Robot.java
@@ -0,0 +1,38 @@
+public class Robot implements Participant {
+    protected int number;
+    protected int jumpHeight;
+    protected int runLength;
+
+    public Robot(int number, int jumpHeight, int runLength) {
+        this.number = number;
+        this.jumpHeight = jumpHeight;
+        this.runLength = runLength;
+    }
+
+
+
+    @Override
+    public void run() {
+
+        System.out.println(String.format("Robot [%s] try runing", number));
+
+
+    }
+
+
+    public void jump() {
+        System.out.println(String.format("Robot [%s] try jumping", number));
+
+    }
+    public int getJumpHeight(){
+        return jumpHeight;
+    }
+    public int getRunLength(){
+        return runLength;
+    }
+    public void win(){
+        System.out.format("Участник Robot %s завершил полосу препятствий\n", number);
+    }
+
+
+}
