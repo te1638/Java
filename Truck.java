46f059c7ba43e9eea2ffa71893ec2af3b69c7874
te1638 <stalker.ksielev2012@yandex.ru>
2020-09-07 20:11:34 +0300

    Урок 5. Многопоточность. Часть II

---
+++ Truck.java
@@ -0,0 +1,44 @@
+public class Truck implements Runnable {
+    private final String type = ("Truck");
+    private final float vol = 60f;
+    private final float expense = 15f;
+    public float amountOfGasoline = vol;
+    private final String name;
+    private FuelStation fuelStation;
+
+    public Truck(String name, FuelStation fuelStation) {
+        this.name = name;
+        this.fuelStation = fuelStation;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(type + " "+ name + " going to road with fuel = "+ vol);
+        while (true){
+            try {
+                Thread.sleep(3000);
+                amountOfGasoline -= expense;
+                System.out.println(type + " " + name + " fuel = " + amountOfGasoline);
+
+
+
+                if (amountOfGasoline <=expense){
+                    System.out.println(type + " "+ name+" going to FuelStation");
+
+                    amountOfGasoline = fuelStation.refuel(vol, type, name);
+
+
+
+                }
+
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
+
+
+
